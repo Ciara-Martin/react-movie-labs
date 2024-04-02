1c8f66259ff43ced433b71b0d4c1894e7c8973c3
Ciara Martin <ciaramartinn18@gmail.com>
2024-04-02 21:08:57 +0100

    Exercise Two: Change upcoming movies page to use caching.

--- movies/src/pages/upcomingMovies.js
+++ movies/src/pages/upcomingMovies.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import PageTemplate from '../components/templateMovieListPage';
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from "react-query";
 import { getUpcoming } from "../api/tmdb-api";
-import { useQuery } from 'react-query';
-import Spinner from '../components/spinner';
+import Spinner from "../components/spinner";
 
-const UpcomingMoviesPage = (props) => {
-    const [upcoming, setUpcoming] = useState([]);
-    const favorites = upcoming.filter(m => m.favorite)
-    localStorage.setItem('favorites', JSON.stringify(favorites))
+    const UpcomingMoviesPage = () => {
+      const { data: upcoming, isLoading, isError, error } = useQuery(
+        "upcomingMovies",
+        getUpcoming
+      );
   
-  
-    useEffect(() => {
-      getUpcoming().then(upcoming => {
-        setUpcoming(upcoming);
-      });
-    }, []);
-
-    const addToFavorites = (movieId) => {
-        const updatedMovies = upcoming.map((m) =>
-          m.id === movieId ? { ...m, favorite: true } : m
-        );
-        setUpcoming(updatedMovies);
+      const addToFavorites = (movieId) => {
+        // Logic to add movie to favorites
+        console.log("Adding movie to favorites:", movieId);
       };
+
+      if (isLoading) {
+        return <Spinner />;
+      }
     
-  
-    return (
-      <PageTemplate
-        title='Discover Upcoming Movies'
-        movies={upcoming}
-        selectFavorite={addToFavorites}
-      />
-    );
-  };
-  export default UpcomingMoviesPage;
+      if (isError) {
+        return <div>Error: {error.message}</div>;
+      }
+    
+      return (
+        <PageTemplate
+          title="Discover Upcoming Movies"
+          movies={upcoming}
+          selectFavorite={addToFavorites}
+        />
+      );
+    };
+    
+    export default UpcomingMoviesPage;
